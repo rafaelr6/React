d8fcd8888c350ccf32124c7dbb528b48497cbd50
rafael rodriguez <rafaelr6@hotmail.com>
2020-03-04 11:54:31 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="False">
+      My Favourite Foods
+    </h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
